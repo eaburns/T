faeda83755518e50211e2a15c5028d4adf662a85
Ethan Burns <burns.ethan@gmail.com>
2019-01-04 21:18:28 -0500

    ui: comment the config variables

--- ui/config.go
+++ ui/config.go
@@ -16,13 +16,27 @@ const (
 )
 
 var (
+	// font is the default font.
 	font, _ = truetype.Parse(goregular.TTF)
-	fontPt  = 11
-	fg      = color.RGBA{R: 0x10, G: 0x28, B: 0x34, A: 0xFF}
-	colBG   = color.RGBA{R: 0xEA, G: 0xEA, B: 0xEA, A: 0xFF}
-	tagBG   = color.RGBA{R: 0xCF, G: 0xE0, B: 0xF7, A: 0xFF}
-	bodyBG  = color.RGBA{R: 0xFE, G: 0xF0, B: 0xE6, A: 0xFF}
-	hiBG1   = color.RGBA{R: 0xCC, G: 0xCD, B: 0xAC, A: 0xFF}
-	hiBG2   = color.RGBA{R: 0xEC, G: 0x90, B: 0x7F, A: 0xFF}
-	hiBG3   = color.RGBA{R: 0xB7, G: 0xE5, B: 0xB2, A: 0xFF}
+
+	// fontPt is the default font size in Postscript Points.
+	fontPt = 11
+
+	// fg is the text foreground color.
+	fg = color.RGBA{R: 0x10, G: 0x28, B: 0x34, A: 0xFF}
+
+	// colBG is the column background color.
+	colBG = color.RGBA{R: 0xEA, G: 0xEA, B: 0xEA, A: 0xFF}
+
+	// tagBG is the tag background color.
+	tagBG = color.RGBA{R: 0xCF, G: 0xE0, B: 0xF7, A: 0xFF}
+
+	// bodyBG is a body background color.
+	bodyBG = color.RGBA{R: 0xFE, G: 0xF0, B: 0xE6, A: 0xFF}
+
+	// hiBG1, hiBG2, and hiBG2 are the background colors
+	// of 1-, 2-, and 3-click highlighted text.
+	hiBG1 = color.RGBA{R: 0xCC, G: 0xCD, B: 0xAC, A: 0xFF}
+	hiBG2 = color.RGBA{R: 0xEC, G: 0x90, B: 0x7F, A: 0xFF}
+	hiBG3 = color.RGBA{R: 0xB7, G: 0xE5, B: 0xB2, A: 0xFF}
 )
