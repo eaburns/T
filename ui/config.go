cb66f286ccaa6876248eb261c67d6a04aa2ad797
Ethan Burns <burns.ethan@gmail.com>
2019-01-09 08:06:00 -0500

    ui: remove the dead padBG variable

--- ui/config.go
+++ ui/config.go
@@ -37,15 +37,11 @@ var (
 	// fg is the text foreground color.
 	fg = color.RGBA{R: 0x10, G: 0x28, B: 0x34, A: 0xFF}
 
-	// colBG is the column background color.
-	colBG = color.White
-
-	// padColor is the padding color.
-	// TODO: remove padColor once it's replaced by frameColor.
-	padColor = color.RGBA{R: 0x10, G: 0x28, B: 0x34, A: 0xFF}
-
 	// frameColor is the lines drawn between columns and rows.
 	frameColor = fg
+
+	// colBG is the column background color.
+	colBG = color.White
 
 	// tagBG is the tag background color.
 	tagBG = color.RGBA{R: 0xCF, G: 0xE0, B: 0xF7, A: 0xFF}
